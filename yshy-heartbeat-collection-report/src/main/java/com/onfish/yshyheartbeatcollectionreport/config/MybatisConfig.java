b06451a16aacaad3bbf7af21c99419e5dee4fd9f
15282810440 <1059708266@qq.com>
2019-07-31 18:11:22 +0800

    分页插件

--- yshy-heartbeat-collection-report/src/main/java/com/onfish/yshyheartbeatcollectionreport/config/MybatisConfig.java
+++ yshy-heartbeat-collection-report/src/main/java/com/onfish/yshyheartbeatcollectionreport/config/MybatisConfig.java
@@ -1,49 +1,31 @@
-//package com.onfish.yshyheartbeatcollectionreport.config;
-//
-//import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
-//import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-//import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-//import org.apache.ibatis.session.SqlSessionFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-//import org.springframework.stereotype.Component;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//@Component
-//public class MybatisConfig {
-//
-////    @Bean("iSqlInjector")
-////    public ISqlInjector getISqlInjector(){
-////        return new DefaultSqlInjector();
-////    }
-//
-//
-//    @Bean("sqlSessionFactory")
-//    @Primary
-//    public SqlSessionFactory sqlSessionFactory(@Autowired @Qualifier("dataSource") DataSource dataSource) throws Exception {
-//
-//        MybatisSqlSessionFactoryBean sqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
-//        sqlSessionFactoryBean.setDataSource(dataSource);
-//        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
-//
-////        Interceptor interceptor = new PageInterceptor();
-////        Properties properties = new Properties();
-////        properties.setProperty("helperDialect", "sqlserver");
-////        properties.setProperty("offsetAsPageNum", "true");
-////        properties.setProperty("rowBoundsWithCount", "true");
-////        properties.setProperty("reasonable", "false");
-////        interceptor.setProperties(properties);
-////        sqlSessionFactoryBean.setPlugins(new Interceptor[] {interceptor});
-//
-//
-//        return sqlSessionFactoryBean.getObject();
-//
-//    }
-//
-//}
+package com.onfish.yshyheartbeatcollectionreport.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@Component
+public class MybatisConfig {
+
+    @EnableTransactionManagement
+    @Configuration
+    @MapperScan("com.baomidou.cloud.service.*.mapper*")
+    public class MybatisPlusConfig {
+
+        /**
+         * 分页插件
+         */
+        @Bean
+        public PaginationInterceptor paginationInterceptor() {
+            PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+            // paginationInterceptor.setLimit(你的最大单页限制数量，默认 500 条，小于 0 如 -1 不受限制);
+            return paginationInterceptor;
+        }
+    }
+
+}
